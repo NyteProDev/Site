3aa853262201b3d2ae9e31383ccbe4be22c67c0d
Nyte <59273613+NyteProDev@users.noreply.github.com>
2025-07-05 00:38:43 +0200

    dza

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ✅ Obligatoire pour Vercel
+  base: '/',
   build: {
     outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true // 🔥 important pour React Router
   }
-})
+});
