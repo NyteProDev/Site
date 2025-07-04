2860fe8c2921886c7a30167b7ca8151bf67a1b99
Nyte <59273613+NyteProDev@users.noreply.github.com>
2025-07-04 22:02:43 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' // ✅ indique le dossier de build pour Vercel
+  },
+  server: {
+    port: 5173,
+    host: true
+  },
+  preview: {
+    port: 8080
+  }
 })
