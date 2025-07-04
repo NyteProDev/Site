cf7ff7808fc52859457fc49f54e0152541863056
Nyte <59273613+NyteProDev@users.noreply.github.com>
2025-07-04 22:11:11 +0200

    dza

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ✅ Ajouté : permet de servir les assets correctement
   build: {
     outDir: 'dist' // ✅ indique le dossier de build pour Vercel
   },
