8afa59c0552a655234707704185d3049558e5d65
Nyte <59273613+NyteProDev@users.noreply.github.com>
2025-07-05 00:49:24 +0200

    dza

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // 👈 ajoute ça
 import App from './App.jsx';
-import { AuthProvider } from './AuthContext.jsx'; // 👈 importe
+import { AuthProvider } from './AuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter> {/* 👈 entoure ici */}
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
